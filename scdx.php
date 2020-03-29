96437284f9d58182d82c66f5db47b943dc72b4fd
zengjian0303 <zengjian0303@126.com>
2020-03-29 17:01:20 +0800

    1

---
+++ scdx.php
@@ -0,0 +1,7 @@
+黑鸟博客,http://cstv.live.wscdns.com/live/xiamen/playlist.m3u8
+厦门卫视,http://cstv.live.wscdns.com/live/xiamen/playlist.m3u8
+厦门一套,http://cstv.live.wscdns.com/live/xiamen1/playlist.m3u8
+厦门二套,http://cstv.live.wscdns.com/live/xiamen2/playlist.m3u8
+厦门三套,http://cstv.live.wscdns.com/live/xiamen3/playlist.m3u8
+厦门四套,http://cstv.live.wscdns.com/live/xiamen4/playlist.m3u8
+WWW.GUIHET.COM,http://cstvpull.live.wscdns.com/live/xiamenyidong.flv
